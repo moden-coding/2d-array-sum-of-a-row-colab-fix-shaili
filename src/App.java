2c46c20e59832e1d2212c6fc7f27ac72a92858c1
Shaili <27ssegre@gannacademy.org>
2026-01-16 12:22:06 -0500

    done

--- src/App.java
+++ src/App.java
@@ -9,7 +9,11 @@ public class App {
 		System.out.println(sumRow(a,0)); //should print 17
 	}
 	public static int sumRow(int[][] array, int row){
-		return -1;
+		int sum = 0;
+		for(int col=0; col<array[row].length; col++){
+			sum += array[row][col];
+		}
+		return sum;
 	}
 	
 }
